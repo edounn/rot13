bf387a6798f7ac401b501804081ef546c17f18d9
eddie_dounn <eddie.dounn@gmail.com>
2017-06-13 21:12:26 -0500

    Removed obj that originally held output.
    Originally had an object that held the conversion thinking it would
    be nice to return multiple conversion types.  Like, return a string,
    array, etc.  Having rot13 output only a string seems to be the best
    most straight forward way to implement a toy problem like this.

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 function rot(str){
-  var rotObj = {};
 	var input = ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz?!';
 	var output =' NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm!?';
 	var trans = '';
-	var outputArr = output.split('');
-	var inputArr = input.split('');
-	
 	for(var i = 0; i < str.length; i++){
 		for(var j = 0; j < input.length; j++){
 			if(str[i] === input[j]){
@@ -13,18 +9,11 @@ function rot(str){
 			}
 		}
 	}
-	rotObj.string = trans;
-	// return rotObj;
-	 return trans
+	return trans
 }
 
-//rot('Jul qvq gur puvpxra pebff gur ebnq!'); //V unir n qbt anzrq ylphf
-//rot('URYYB ') // HELLO
-//rot('Why did the chicken cross the road? Gb trg gb gur bgure fvqr!');  //Jul qvq gur puvpxra pebff gur ebnq? To get to the other side!
-
-
+//testing
 function assertEqual(actual, expected, testName) {
-  // your code here
   if( actual === expected){
     console.log('PASS [ ' + testName + ' ] expected, "' + expected + '" AND DID get " '+ actual + ' " \n');
   }else{
